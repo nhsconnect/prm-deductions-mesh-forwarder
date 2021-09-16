afbea62df7df0560690a1eaa145cb3926f8046e4
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-09-16 15:09:36 +0300

    [PRMT-2280] Rename ecs service

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -4,7 +4,7 @@ locals {
   private_subnets = split(",", data.aws_ssm_parameter.deductions_private_private_subnets.value)
 }
 
-resource "aws_ecs_service" "ecs-service" {
+resource "aws_ecs_service" "mesh_forwarder" {
   name            = "${var.environment}-${var.component_name}-service"
   cluster         = local.ecs_cluster_id
   task_definition = aws_ecs_task_definition.task.arn
