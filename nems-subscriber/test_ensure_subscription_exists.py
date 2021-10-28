8c2b72bd4c981f430be90704debe594b9e3b7e44
Dan Moore <dan.moore1@nhs.net>
2021-10-28 17:29:10 +0100

    [PRMT-2382] Fix pipeline; introduce local integration test

--- nems-subscriber/test_ensure_subscription_exists.py
+++ nems-subscriber/test_ensure_subscription_exists.py
@@ -1,16 +1,6 @@
-from dataclasses import dataclass
-
 from unittest.mock import Mock
 
-
-@dataclass
-class Config:
-    asid: str
-    ods_code: str
-    api_host: str
-    mesh_mailbox_id: str
-    nems_subscription_id: str
-
+from subscribe import Config
 
 def ensure_subscription_exists(subscription_reader, subscription_creator, config: Config):
     if not subscription_reader().status_code == 200:
@@ -19,7 +9,7 @@ def ensure_subscription_exists(subscription_reader, subscription_creator, config
 
 def test_that_when_subscription_does_not_exist_it_is_created():
     config = Config(asid="A1", ods_code="ods", api_host="host", mesh_mailbox_id="mesh", nems_subscription_id="sub")
-    reader = Mock(status_code=200)
+    reader = Mock(status_code=404)
     creator = Mock()
 
     ensure_subscription_exists(reader, creator, config)
@@ -27,11 +17,12 @@ def test_that_when_subscription_does_not_exist_it_is_created():
     creator.assert_called_once_with(config.asid, config.ods_code, config.api_host, config.mesh_mailbox_id)
 
 def test_that_when_subscription_does_exist_it_is_not_recreated():
+    config = Config(asid="A1", ods_code="ods", api_host="host", mesh_mailbox_id="mesh", nems_subscription_id="sub")
     a_subscription={'name': 'bob'}
-    reader = Mock(return_value=a_subscription)
+    reader = Mock(return_value=Mock(status_code=200))
     creator = Mock()
 
-    ensure_subscription_exists(reader, creator)
+    ensure_subscription_exists(reader, creator, config)
 
     creator.assert_not_called()
 
