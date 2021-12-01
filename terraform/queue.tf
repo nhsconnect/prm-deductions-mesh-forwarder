5c5b13d3432e19622b994aa729a33b428160a211
Mario Giampietri <mario.giampietri1@nhs.net>
2021-12-01 17:11:39 +0000

    [PRMT-2311] another fix for nems observability queue ssm param, referenced via id not value

--- terraform/queue.tf
+++ terraform/queue.tf
@@ -85,7 +85,12 @@ resource "aws_sqs_queue" "observability" {
 
 resource "aws_ssm_parameter" "nems_events_observability_queue" {
   name = "/repo/${var.environment}/output/${var.component_name}/nems-events-observability-queue"
-  value = aws_sqs_queue.nems_events_observability_queue.value
+  type = "String"
+  value = aws_sqs_queue.observability.id
+  tags = {
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
 }
 
 resource "aws_sns_topic_subscription" "nems_events_to_observability_queue" {
